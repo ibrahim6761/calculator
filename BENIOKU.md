dba18f147aff69e7a2808a1d146f20b4af389752
İbrahim Ethem Bakkal <ibrahimethembakkal@Ibrahim-MacBook-Air.local>
2021-04-23 12:17:12 +0300

    calculator

---
+++ BENIOKU.md
@@ -0,0 +1,23 @@
+# Hesap Makinesi
+
+## Projeyi Kurma 
+
+```
+npm install
+```
+
+### Proje Çalıştırma
+
+```
+npm run serve
+```
+
+### Listeleme ve Dosyaları Fixleme
+
+```
+npm run lint
+```
+
+###
+
+Git hub adresime [Buradan](https://github.com/ibrahim6761
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hesapMakinesi></hesapMakinesi>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import hesapMakinesi from "@/components/hesapMakinesi";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    hesapMakinesi : hesapMakinesi
   }
 }
 </script>
